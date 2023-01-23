b11249f83f5d46b67816f61295be0b23092f118f
vermanica <veronicaann621@gmail.com>
2023-01-23 16:00:57 -0500

    updated homework with comments for clarity

--- Homework1/Bayesian_HW1_VAW.R
+++ Homework1/Bayesian_HW1_VAW.R
@@ -7,6 +7,8 @@
 rm(lsit = ls())
 gc()
 
+set.seed(0519)
+
 # References: "ImportanceSampling.r" & "ImporanceSampling_pt2.r"
 
 # Function ----
@@ -110,13 +112,14 @@ summary(w)
 sum(w)
 
 ## Effective sample size
-ESS=1/sum(w^2)
+ESS = 1/sum(w^2)
 ESS
+# [1] 7926.4
 
 ## Resampling
-N=10000
-u=sample(u.star,size=N,replace=TRUE,prob=w)
-hist(u,prob=TRUE,breaks=30,main=paste("ESS=",round(ESS)))
+N = 10000
+u = sample(u.star, size = N, replace = TRUE, prob = w)
+hist(u, prob = TRUE, breaks = 30, main = paste ("ESS =", round(ESS)))
 
 # Finally, let's plot the results.....
 hist(u,prob=TRUE,breaks=30,main=paste("ESS=",round(ESS)),xlim=c(0,2))
@@ -132,16 +135,18 @@ legend("topright",legend=c("SIS Histogram","Prior","Posterior"),
 # D. Report the mean and marginal 95% credible intervals for all parameters. 
 a.post = a+length(airplane)
 b.post = b+sum(airplane)
+
 post.mean.analytic = a.post/b.post
+# [1] 0.3064645
 
 # ANSWERS ----
 # Mean: Analytics
 mean(u)
-# 0.3065115
+# 0.3066594
 
 # Mean: Using importance sampling weights
 sum(w*u)
-# 0.3062683
+# 0.306537
 
 # CI: Analytics
 qgamma(c(.025,.975),shape=a.post,rate=b.post)
@@ -150,14 +155,11 @@ qgamma(c(.025,.975),shape=a.post,rate=b.post)
 ## CI: Using samples
 quantile(u,c(.025,.975))
 #     2.5%      97.5% 
-# 0.2491419  0.3700360
+# 0.2501406 0.3706314 
 
 ########### End Question 2 ##############
 
 ########### Solution: Question 3 ##############
-rm(list = ls())
-gc()
-
 # Let y_i be the number of sick days that a person takes due to an illness, 
 # and let x_i be the number of months that person has been taking part in a 
 # treatment program.  
@@ -173,79 +175,91 @@ gc()
 ## b1~N(0, 4)
 ## b2~N(0, 4)
 
+## Format the data ----
 x = c(8,14,11,7,32,8,28,21,27,15,26,13,19,22,15,12,15,7,9,15,26,22,16,12,6)
 y = c(5,2,5,4,1,3,0,2,1,2,2,5,3,2,1,2,2,8,5,2,1,1,6,4,3)
 
-# hyperparameters
-a = 1
-b = 1
+## [1] draw M RVs from the prior (normal) ----
+M = 10^6
+a.star = rnorm(M, 0, sqrt(4)) #a param
+b.star = rnorm(M, 0, sqrt(4)) #b param
 
-## [1] draw M RVs from the prior (normal)
-M = 10^5
-b0.star=rnorm(M, 0, sqrt(4))
-b1.star=rnorm(M, 0, sqrt(4))
-
-## [2] calculate weights
+## [2] calculate weights ----
 log.q = rep(NA, M)
 
 for(m in 1:M){
-  p = exp(b0.star[m] + b1.star[m]*x)
+  p = exp(a.star[m] + b.star[m]*x) 
   log.q[m]=sum(dpois(y, lambda = p, log=TRUE))
 }
 
-log.denom=logsumexp(log.q)
+log.denom = logsumexp(log.q)
 
-log.w=log.q-log.denom
-w=exp(log.w)
+log.w = log.q-log.denom
+w = exp(log.w)
 summary(w)
 sum(w)
 
-ESS=1/sum(w^2)
+## [2] Check effective sample size (ESS) ----
+ESS = 1/sum(w^2)
 ESS
+# [1] 639.9817
 
 ## resampling
-N=500
+N = 500
 ## resample the INDEX (draw from the prior for all three parameters)
-idx=sample(1:M,size=N,replace=TRUE,prob=w)
+idx = sample(1:M,size = N, replace = TRUE, prob = w)
 
-b0=b0.star[idx]
-b1=b1.star[idx]
+a = a.star[idx]
+b = b.star[idx]
 
-hist(b0)
-hist(b1)
+hist(a)
+hist(b)
 
-## posterior inference
-post.mat=cbind(b0,b1)
-post.mean=apply(post.mat,2,mean)
-post.CI=apply(post.mat,2,quantile,c(.025,.975))
+## [3a.] Posterior inference ----
+post.mat = cbind(a, b)
+post.mean = apply(post.mat,2,mean)
+post.CI = apply(post.mat,2,quantile,c(.025,.975))
 
 # ANSWER: Find posterior mean ----
 post.mean
-#       b0          b1 
-# 2.10631000 -0.07312266
+#       a          b 
+# 2.06594295 -0.07113701 
 
 # ANSWER: Marginal 95% CI ----
 post.CI
-#             b0          b1
-# 2.5%  1.488859 -0.11624149
-# 97.5% 2.702278 -0.03320681
+#              a           b
+# 2.5%  1.454949 -0.10781549
+# 97.5% 2.587724 -0.03287357
 
 
 # Using the posterior predictive distribution, provide a 95% prediction interval 
 # on the number of sick days that might be taken in a year for a person who has 
 # been on the treatment program for 24 months.  
-mn.ppd_1=b0+b1*24
-hist(mn.ppd_1,main="Posterior Predictive Mean: Number of sick days, 24 mo.")
+mn.ppd_1 = exp(a + b*24)
+ci.24 <- quantile(mn.ppd_1, c(0.025, 0.0975))
+#       2.5%    9.75% 
+#   0.8833687 1.0671182 
+mean.24 <- mean(mn.ppd_1)
+# [1] 1.472093
 
+hist(mn.ppd_1,main="Posterior Predictive Mean: Number of sick days, 24 mo.")
 summary(mn.ppd_1) 
-#     Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# -7.2392 -4.7531 -4.1950 -4.2671 -3.5749 -0.6076 
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#  0.4831  1.2110  1.4510  1.4721  1.7039  2.8938  
 
 # Provide a similar 95% prediction interval for a person 
 # who has been on the treatment program for 36 months
-mn.ppd_2=b0+b1*36
+mn.ppd_2 = exp(a + b*36)
+
+ci.36 <- quantile(mn.ppd_2, c(0.025, 0.0975))
+#       2.5%     9.75% 
+#   0.2570704 0.3476915  
+mean.36 <- mean(mn.ppd_2)
+# [1] 0.6715566
 hist(mn.ppd_2, main="Posterior Predictive Mean: Number of sick days, 36 mo.")
 summary(mn.ppd_2)
 #     Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-# -7.1902 -4.7145 -4.1497 -4.2331 -3.5638 -0.5735
+# 0.08509 0.45122 0.61119 0.67156 0.82567 1.92369  
 
+
+### END HW 1 ###
